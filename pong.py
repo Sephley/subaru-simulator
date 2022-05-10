4c256653511049bb16786587cd02801e4e610b4c
Sephley <hurley.joseph@protonmail.ch>
2022-05-10 21:58:36 +0200

    added new background
    added new background

--- pong.py
+++ pong.py
@@ -15,9 +15,9 @@ pygame.display.set_caption('SUUUUUUUUBARU')
 # Sets background
 background_size = width, height = (1920, 1080)
 background_surface = pygame.Surface(background_size)
-background = pygame.image.load('C:/Users/Sephley/Documents/GitHub/subaru-simulator/test_image.jpg')
+background = pygame.image.load('C:/Users/Sephley/Documents/GitHub/subaru-simulator/Hintergrund.png')
 screen.blit(background, (0, 0))
-pygame.display.update()  # or pygame.display.flip()
+pygame.display.update()
 
 while True:
     # Takes input from User
